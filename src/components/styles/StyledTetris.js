f49327153f2a331d0e8abfd45c043c10510da161
Bennis99 <benjamin.kodehode@gmail.com>
2021-11-02 09:50:48 +0100

    fix

--- src/components/styles/StyledTetris.js
+++ src/components/styles/StyledTetris.js
@@ -1,7 +1,5 @@
 import styled from "styled-components"
-import bgImage from "../../img/anime.jpg"
-//bg.png
-//anime.jpg
+import bgImage from "../../img/bg.png"
 
 export const StyledTetrisWrapper = styled.div`
     width: 100vw;
