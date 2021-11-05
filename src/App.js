761d490afa3699cd7380f4362a6c018df3e84a30
Bennis99 <benjamin.kodehode@gmail.com>
2021-11-05 13:09:31 +0100

    k

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import Tetris from "./components/Tetris"
 function App() {
     return (
     <div>
+        <head>
+            <title>React Tetris</title>
+            <meta name="description" content="Created by Benjamin" />
+            <link rel="icon" href="./img/tetrisikon.png" />
+            <meta property="og:image" content="./img/tetrisikon.png" />
+        </head>
         <Tetris />
     </div>
     )
